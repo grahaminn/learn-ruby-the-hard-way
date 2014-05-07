0fc4546ea9891535b7ce65d482ce9d1ef224612c
grahaminn <grahaminn@hotmail.com>
2014-05-07 20:13:16 +0100

    some more exercises

---
+++ ex3.rb
@@ -0,0 +1,30 @@
+# Prints an intro to the user on stdout
+puts "I will now count my chickens:"
+
+# Prints "Hens 30" to stdout
+puts "Hens", 25 + 30 / 6
+
+# Prints "Roosters 97" to stdout
+puts "Roosters", 100 - 25 * 3 % 4
+
+# Self explanatory
+puts "Now I will count the eggs:"
+
+# Long-winded bidmas test. 3 + 2 + 1 - 5 + (4 % 2) - (1 / 4) + 6, prints 7 to stdout
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# print false to stdout
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
