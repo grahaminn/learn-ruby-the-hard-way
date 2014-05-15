50bf1f045afb89a6133c5410ec26bd5cf9497ed2
grahaminn <grahaminn@hotmail.com>
2014-05-15 23:55:03 +0100

    moar exercises

---
+++ ex31.rb
@@ -0,0 +1,56 @@
+def prompt
+  print "> "
+end
+
+puts "You enter a dark room with two doors.  Do you go through door #1, door #2 or door #3?"
+
+prompt; door = gets.chomp
+
+if door == "1"
+  puts "There's a giant bear here eating a cheese cake.  What do you do?"
+  puts "1. Take the cake."
+  puts "2. Scream at the bear."
+
+  prompt; bear = gets.chomp
+
+  if bear == "1"
+    puts "The bear eats your face off.  Good job!"
+  elsif bear == "2"
+    puts "The bear eats your legs off.  Good job!"
+  else
+    puts "Well, doing #{bear} is probably better.  Bear runs away."
+  end
+
+elsif door == "2"
+  puts "You stare into the endless abyss at Cthuhlu's retina."
+  puts "1. Blueberries."
+  puts "2. Yellow jacket clothespins."
+  puts "3. Understanding revolvers yelling melodies."
+
+  prompt; insanity = gets.chomp 
+
+  if insanity == "1" or insanity == "2"
+    puts "Your body survives powered by a mind of jello.  Good job!"
+  else
+    puts "The insanity rots your eyes into a pool of muck.  Good job!"
+  end
+
+elsif door == "3"
+  puts "Yog-sothoth cavorts with Lavinia Whately, what do you do ?"
+  puts "1. Unsee it"
+  puts "2. Stick of dynamite in the beak."
+  puts "3. Join in."
+ 
+  prompt; desperation = gets.chomp
+  
+  if desperation == "1"
+    puts "You cannot unsee it"
+  elsif desperation == "2"
+    puts "The dynamite explodes in your hand before you stick it in the beak. Oops"
+  else
+    puts "You're sick."
+  end
+
+else
+  puts "You stumble around and fall on a knife and die.  Good job!"
+end
